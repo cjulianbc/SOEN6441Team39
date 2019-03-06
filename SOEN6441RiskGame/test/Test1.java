9e4d9ce09dd3a8e8fdf32804df5384aa5ffb140f
ariesabhi55 <aries.abhi5@gmail.com>
2019-03-06 15:36:10 -0500

    4 test cases added for map validation

--- SOEN6441RiskGame/test/Test1.java
+++ SOEN6441RiskGame/test/Test1.java
@@ -82,6 +82,59 @@ class Test1 {
 		  "Canada,143,28,North America,USA,India";
 		  assertFalse(file.validateMap(continents, countries)); } 
 	 
+	 @Test void testMapInvalidCoordinateFormat() {
+			continents="Asia=2"+"\n"+"North America=8"; countries=
+		  "India,143,28,Asia,Srilanka,Bangladesh,Pakistan,Nepal,Bhutan,Canada,China\n"+
+		  "Pakistan,27,34,Asia,Afghanistan,China,India\n" +
+		  "Srilanka,45,66,Asia,India\n" + "Bangladesh,33,98,Asia,India,Bhutan\n" +
+		  "Nepal,107,97,Asia,India,Bhutan,China\n" +
+		  "Bhutan,120,45,Asia,India,Nepal,Bangladesh,China\n" +
+		  "China,17,89,Asia,India,Pakistan,Nepal,Bhutan\n" +
+		  "Afghanistan,29,88,Asia,Pakistan\n" + 
+		  "USA,9,9,North America,Canada\n" +
+		  "Canada,India,USA,North America,USA,India";
+		  assertFalse(file.validateMap(continents, countries)); }
+	 
+	 @Test void testMapInvalidAdjacenectCountry() {
+			continents="Asia=2"+"\n"+"North America=8"; 
+			countries="India,143,28,Asia,Srilanka,Bangladesh,Pakistan,Nepal,Bhutan,Canada,China,Ethiopia\n"+
+		  "Pakistan,27,34,Asia,Afghanistan,China,India\n" +
+		  "Srilanka,45,66,Asia,India\n" + "Bangladesh,33,98,Asia,India,Bhutan\n" +
+		  "Nepal,107,97,Asia,India,Bhutan,China\n" +
+		  "Bhutan,120,45,Asia,India,Nepal,Bangladesh,China\n" +
+		  "China,17,89,Asia,India,Pakistan,Nepal,Bhutan\n" +
+		  "Afghanistan,29,88,Asia,Pakistan\n" + 
+		  "USA,9,9,North America,Canada\n" +
+		  "Canada,69,69,North America,USA,India";
+		  assertFalse(file.validateMap(continents, countries)); }
+	 
+	 @Test void testMapInvalidUniDirectionalConnection() {
+			continents="Asia=2"+"\n"+"North America=8"; 
+			countries="India,143,28,Asia,Srilanka,Bangladesh,Pakistan,Nepal,Bhutan,Canada,China\n"+
+		  "Pakistan,27,34,Asia,Afghanistan,China,India\n" +
+		  "Srilanka,45,66,Asia,India\n" + "Bangladesh,33,98,Asia,India,Bhutan\n" +
+		  "Nepal,107,97,Asia,India,Bhutan,China\n" +
+		  "Bhutan,120,45,Asia,India,Nepal,Bangladesh,China\n" +
+		  "China,17,89,Asia,India,Pakistan,Nepal,Bhutan\n" +
+		  "Afghanistan,29,88,Asia,Pakistan\n" + 
+		  "USA,9,9,North America,Canada\n" +
+		  "Canada,69,69,North America,USA";
+		  assertFalse(file.validateMap(continents, countries)); }
+	 
+	 @Test void testMapInvalidDisconnectedGraph() {
+			continents="Asia=2"+"\n"+"North America=8"; 
+			countries="India,143,28,Asia,Srilanka,Bangladesh,Pakistan,Nepal,Bhutan,China\n"+
+		  "Pakistan,27,34,Asia,Afghanistan,China,India\n" +
+		  "Srilanka,45,66,Asia,India\n" + "Bangladesh,33,98,Asia,India,Bhutan\n" +
+		  "Nepal,107,97,Asia,India,Bhutan,China\n" +
+		  "Bhutan,120,45,Asia,India,Nepal,Bangladesh,China\n" +
+		  "China,17,89,Asia,India,Pakistan,Nepal,Bhutan\n" +
+		  "Afghanistan,29,88,Asia,Pakistan\n" + 
+		  "USA,9,9,North America,Canada\n" +
+		  "Canada,69,69,North America,USA";
+		  assertFalse(file.validateMap(continents, countries)); }
+	 
+	 
 	 
 
 }
