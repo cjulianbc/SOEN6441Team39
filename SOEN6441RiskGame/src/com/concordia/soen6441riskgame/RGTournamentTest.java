fb2eb4358b7c33cf2d02d3748a8c4518064c1cb8
Nesar Alam <nesaralam910@gmail.com>
2019-04-09 15:59:50 -0400

    tournament test class

--- SOEN6441RiskGame/src/com/concordia/soen6441riskgame/RGTournamentTest.java
+++ SOEN6441RiskGame/src/com/concordia/soen6441riskgame/RGTournamentTest.java
@@ -24,7 +24,7 @@ public class RGTournamentTest {
 	}
 	
 	@Test
-	public void testTournamnet() {
+	public void testTournament() {
 		tournament.startTournament();
 		assertEquals(6,tournament.getResultsSize());
 	}
