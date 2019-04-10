023e08d9e1dde4ca6166e091c8ea379068f3f739
amit <almighty000@gmail.com>
2019-04-09 21:18:53 -0400

    genarated javadoc

--- SOEN6441RiskGame/src/com/concordia/soen6441riskgame/RGCapturedTerritoryFrame.java
+++ SOEN6441RiskGame/src/com/concordia/soen6441riskgame/RGCapturedTerritoryFrame.java
@@ -1,19 +1,15 @@
 package com.concordia.soen6441riskgame;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
@@ -54,7 +50,7 @@ public class RGCapturedTerritoryFrame extends JFrame {
 		lblSetupPhase.setBounds(159, 37, 141, 33);
 		moveArmyAttackPhase.add(lblSetupPhase);
 		
-		JLabel lblCurrent = new JLabel("Current: "+currentPlayerName);
+		JLabel lblCurrent = new JLabel("Current: " + currentPlayerName); // showing the current player name
 		lblCurrent.setBounds(35, 91, 113, 23);
 		moveArmyAttackPhase.add(lblCurrent);
 		
